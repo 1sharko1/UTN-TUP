2c1a48314c009cf53df3c8fa25ffe0555a3a30fe
1sharko1 <joa.escobar04@gmail.com>
2025-04-05 05:39:27 -0300

    Actualizado

--- 06 Datos complejos/Ejercicio 6.03.py
+++ 06 Datos complejos/Ejercicio 6.03.py
@@ -2,7 +2,7 @@ precios_frutas = {
     'Banana': 1330, 
     'Ananá': 2500, 
     'Melón': 3000, 
-    'Uva':1450,
+    'Uva': 1450,
     "Naranja" : 1200,
     "Manzana" : 1700,
     "Pera" : 2800,
