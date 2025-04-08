9d98950e4a9b001fb857e7daab583e105f44adb1
1sharko1 <joa.escobar04@gmail.com>
2025-04-07 21:53:46 -0300

    Prueba

--- 04 Estructuras Repetitivas/Ejercicio 4.01.py
+++ 04 Estructuras Repetitivas/Ejercicio 4.01.py
@@ -1,4 +1,3 @@
-# Creamos un bucle que recorre desde x (0 por defecto) hasta el parametro final teniendo en cuenta que la fucion range excluye el ultimo numero.
-# Cada iteracion mostrara en pantalla el aumento del valor de la variable x, lo que dara como resultado una lista de los numeros entre 0 y 100.
+# Creamos un bucle que recorre desde x (0 por defecto) hasta el parametro final teniendo en cuenta que la fucion range excluye el ultimo numero.  Cada iteracion mostrara en pantalla el aumento del valor de la variable x, lo que dara como resultado una lista de los numeros entre 0 y 100.
 for x in range (101):
-    print(x)
+    print(x) 
